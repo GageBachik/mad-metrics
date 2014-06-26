a7cbfe65afed6704392d4287ed2b956b0d9215c3
gbachik <gbachik@gmail.com>
2014-06-26 12:08:08 -0600

    Finished Percentage Viewed

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 $(document).on('ready', function() {
-  
+
+	var percentageViewed = ((((window.scrollY + $(window).height()) / $(document).height())*100).toFixed(2));
+	$(document).on('scroll', function(){
+		var formatted = ((((window.scrollY + $(window).height()) / $(document).height())*100).toFixed(2));
+
+		if (formatted > percentageViewed){
+			percentageViewed = formatted;
+		}
+		// console.log(percentageViewed);
+	});
+
 });
