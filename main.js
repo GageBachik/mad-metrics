f2cf7bec6808e5fa5aa36391a49cc268b9f51344
gbachik <gbachik@gmail.com>
2014-06-26 14:23:59 -0600

    added total distance scrolled

--- main.js
+++ main.js
@@ -3,18 +3,20 @@ $(document).on('ready', function() {
 	var percentageViewed = ((((window.scrollY + $(window).height()) / $(document).height())*100).toFixed(2));
 	var initialScroll = window.scrollY;
 
+	
 	$(document).on('scroll', function(){
 		var formatted = ((((window.scrollY + $(window).height()) / $(document).height())*100).toFixed(2));
 		var movedTo = window.scrollY;
-
 		if(initialScroll !== movedTo){
 			distanceScrolled += Math.abs(initialScroll-movedTo);
 			console.log(distanceScrolled);
+			initialScroll = movedTo;
 		}
 		if (formatted > percentageViewed){
 			percentageViewed = formatted;
 		}
 		// console.log(percentageViewed);
+		// console.log(distanceScrolled);
 	});
 
 });
