6ed78d3a6fa91fee9c04362a9c77580d8e8764a3
gbachik <gbachik@gmail.com>
2014-06-26 13:56:22 -0600

    fixing logic

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 $(document).on('ready', function() {
-
+	var distanceScrolled = 0;
 	var percentageViewed = ((((window.scrollY + $(window).height()) / $(document).height())*100).toFixed(2));
+	var initialScroll = window.scrollY;
+
 	$(document).on('scroll', function(){
 		var formatted = ((((window.scrollY + $(window).height()) / $(document).height())*100).toFixed(2));
+		var movedTo = window.scrollY;
 
+		if(initialScroll !== movedTo){
+			distanceScrolled += Math.abs(initialScroll-movedTo);
+			console.log(distanceScrolled);
+		}
 		if (formatted > percentageViewed){
 			percentageViewed = formatted;
 		}
