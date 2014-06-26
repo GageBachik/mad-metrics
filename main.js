ae67e1553f827a7449987cd98f0bab909127f416
gbachik <gbachik@gmail.com>
2014-06-26 16:34:18 -0600

    Finished time spent on page and before clickking sign up

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 $(document).on('ready', function() {
-	var distanceScrolled = 0;
-	var percentageViewed = ((((window.scrollY + $(window).height()) / $(document).height())*100).toFixed(2));
-	var initialScroll = window.scrollY;
+	setTimeout(function(){
+		var distanceScrolled = 0;
+		var percentageViewed = ((((window.scrollY + $(window).height()) / $(document).height())*100).toFixed(2));
+		var initialScroll = window.scrollY;
+		var timeSpent = 0;
+		var timeBeforeSignUp = 0;
 
-	
-	$(document).on('scroll', function(){
-		var formatted = ((((window.scrollY + $(window).height()) / $(document).height())*100).toFixed(2));
-		var movedTo = window.scrollY;
-		if(initialScroll !== movedTo){
-			distanceScrolled += Math.abs(initialScroll-movedTo);
-			console.log(distanceScrolled);
-			initialScroll = movedTo;
-		}
-		if (formatted > percentageViewed){
-			percentageViewed = formatted;
-		}
-		// console.log(percentageViewed);
-		// console.log(distanceScrolled);
-	});
+		var pause = setInterval(function(){
+			timeSpent++;
+		},1000);
 
+
+		$(document).on('scroll', function(){
+			var formatted = ((((window.scrollY + $(window).height()) / $(document).height())*100).toFixed(2));
+			var movedTo = window.scrollY;
+			if(initialScroll !== movedTo){
+				distanceScrolled += Math.abs(initialScroll-movedTo);
+				console.log(distanceScrolled);
+				initialScroll = movedTo;
+			}
+			if (formatted > percentageViewed){
+				percentageViewed = formatted;
+			}
+			// console.log(percentageViewed);
+			// console.log(distanceScrolled);
+			// clearTimeout(pause);
+		});
+
+		$('a').on('click', function(){
+			if(timeBeforeSignUp === 0){
+				timeBeforeSignUp = timeSpent;
+			}
+		});
+	},1);
 });
